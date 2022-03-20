d0ebb3f5d1f0e5593f9fe9ac5a1be596bb00e1e0
Xi Chen <chenxi.edu1997@gmail.com>
2022-03-20 16:35:19 -0500

    login system work but the registration partially work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # This instruction told the controller to create the database of friends automatically.
   resources :friends
   
   root 'home#index'
   get 'home/index'
   get 'home/about'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
