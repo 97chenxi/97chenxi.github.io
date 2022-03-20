86b1b19202347e3a0c7cea14f21826c4b89c5c60
Xi Chen <chenxi.edu1997@gmail.com>
2022-03-20 17:01:42 -0500

    repair the redirect after sign up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+# app/controllers/registrations_controller.rb
+# used for repair the redirecting after sign up.
+
+class RegistrationsController < Devise::RegistrationsController
+    private
+  
+    def after_sign_up_path_for(resource)
+      root_path # or any other path
+    end
+    
+  end
